e30cda9114b09a26db8b9c2c22907d65cfa580a7
Thomas Lukasiewicz <143830652+ThomasL25@users.noreply.github.com>
2025-09-08 01:20:24 -0400

    Create processManagementFour.ps1
    4) Write a PowerShell script that 
         a) If an instance of it is not running already, it starts Google Chrome web browser and directs it Champlain.edu
         b) If an instance is already running, stops it

---
+++ week2/processManagementFour.ps1
@@ -0,0 +1,6 @@
+$chromeOnline = Get-Process -Name "chrome" -ErrorAction SilentlyContinue
+if ($chromeOnline) {
+    Stop-Process -Name "chrome" -Force
+} else {
+    Start-Process "chrome.exe" "https://www.champlain.edu"
+}
