cfbf57b469986939c6fb89673b3ba038431aab37
Thomas L <thomas.lukasiewicz@mymail.champlain.edu>
2025-12-08 18:54:13 -0500

    Ubuntu Apache Logs

---
+++ week11/lecture.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+allLogs=""
+file="/var/log/apache2/access.log"
+
+function getAllLogs(){
+    allLogs=$(cat "$file" | awk '{print $1, $4, $7}' | tr -d "[")
+}
+
+function ips(){
+    ipsAccessed=$(echo "$allLogs" | cut -d' ' -f1)
+}
+
+function pageCount(){
+    pagesAccessed=$(grep -oP '\"GET \K[^ ]+' "$file" | sort | uniq -c)
+}
+
+getAllLogs
+pageCount
+echo "$pagesAccessed"
